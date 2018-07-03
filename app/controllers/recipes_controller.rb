00c1fbee1683f521027680fa0568b87bf90693a9
eduardo.ekoch <eduardo.ekoch@gmail.com>
2018-07-03 19:18:17 -0300

    Adiciona receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,19 @@ class RecipesController < ApplicationController
     id = params[:id]
     @recipe = Recipe.find(id)
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    redirect_to recipe_path(@recipe.id)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :cuisine, :recipe_type)
+  end
 end
