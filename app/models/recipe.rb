6a9989fa8aa43f9e498ebc758c3e8829c1bf9902
eduardo.ekoch <eduardo.ekoch@gmail.com>
2018-07-06 21:47:55 -0300

    Usuário envia foto da receita

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   belongs_to :cuisine
   validates :title, :difficulty, :cook_time, :ingredients, :cook_method, presence: true
   belongs_to :recipe_type
+  has_attached_file :photo
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
